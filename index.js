6aefe89e987961155c161992b8c9ce628524351a
Ricky <gomez.ricky@gmail.com>
2025-10-23 21:36:03 -0500

    updated state

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
 const COHORT = "/2509-PT-Ricky";
 const RESOURCE = "/Events";
-const API = BASE + COHORT + RESOURCE;
+const API = BASE + COHORT +;
 
 // State
 let parties = [];
 let selectedParty;
-let rsvps = [];
-let guests = [];
 
 // Components
 
