8245adb51de4f8cde220a2a7c1dd7dcebefb5994
Ricky <gomez.ricky@gmail.com>
2025-10-23 21:18:33 -0500

    initial commit with API and State Arrays

---
+++ index.js
@@ -0,0 +1,14 @@
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2509-PT-Ricky";
+const RESOURCE = "/Events";
+const API = BASE + COHORT + RESOURCE;
+
+// State
+let parties = [];
+let selectedParty;
+let rsvps = [];
+let guests = [];
+
+// Components
+
+//Render
